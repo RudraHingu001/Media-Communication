396fdb49b269985c6c098f3d18fcffe22b28c581
Rudra Hingu <rudra.agumentik@gmail.com>
2025-12-19 20:39:22 +0530

    Route

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import cors from 'cors';
-
+import connectDB from './src/config/db.js';
 import authRoutes from './routes/auth.routes.js';
 import projectRoutes from './routes/project.routes.js';
 import categoryRoutes from './routes/category.routes.js';
 
 const app = express();
+
+connectDB();
 
 app.use(cors());
 app.use(express.json());
