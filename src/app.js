bdab1ecb4713346bbaa41ee68c059bb6086b9345
Rudra Hingu <rudra.agumentik@gmail.com>
2025-12-19 23:02:09 +0530

    Route change

--- src/app.js
+++ src/app.js
@@ -1,31 +1,22 @@
 import express from 'express';
 import cors from 'cors';
+
 import authRoutes from './routes/auth.routes.js';
 import projectRoutes from './routes/project.routes.js';
 import categoryRoutes from './routes/category.routes.js';
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+app.options('*', cors());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/projects', projectRoutes);
 app.use('/api/categories', categoryRoutes);
 
 app.get('/', (req, res) => {
-    res.status(200).json({
-        message: 'API is running successfully 🚀'
-    });
-});
-
-
-app.get('/db-test', async (req, res) => {
-  try {
-    await connectDB();
-    res.json({ success: true, message: 'MongoDB connected' });
-  } catch (err) {
-    res.status(500).json({ success: false, error: err.message });
-  }
+  res.status(200).json({ message: 'API is running successfully 🚀' });
 });
 
 export default app;
