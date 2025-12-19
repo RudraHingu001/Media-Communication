a9b50356943d6986eb585408b0a6c838fad0a335
Rudra Hingu <rudra.agumentik@gmail.com>
2025-12-19 20:15:01 +0530

    Route change

--- src/app.js
+++ src/app.js
@@ -14,4 +14,10 @@ app.use('/api/auth', authRoutes);
 app.use('/api/projects', projectRoutes);
 app.use('/api/categories', categoryRoutes);
 
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'API is running successfully 🚀'
+    });
+});
+
 export default app;
