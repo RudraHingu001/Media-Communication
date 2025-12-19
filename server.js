ef32a75735ac491efe8d8ab48f172e869104c032
Rudra Hingu <rudra.agumentik@gmail.com>
2025-12-19 20:34:52 +0530

    Route Change

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ import app from './src/app.js';
 dotenv.config();
 connectDB();
 
+app.get('/db-test', async (req, res) => {
+  try {
+    await connectDB();
+    res.json({ success: true, message: 'MongoDB connected' });
+  } catch (err) {
+    res.status(500).json({ success: false, error: err.message });
+  }
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
